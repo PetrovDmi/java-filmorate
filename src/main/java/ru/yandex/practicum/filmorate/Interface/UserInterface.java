ebb643ab6e90e6ab5327287de3a6132471a1efe5
Petrov Dmitry <dima000killer@gmail.com>
2023-02-21 22:11:28 +0500

    6 комит, поправлены замечания, добавлены интерфейсы для контроллеров

--- src/main/java/ru/yandex/practicum/filmorate/Interface/UserInterface.java
+++ src/main/java/ru/yandex/practicum/filmorate/Interface/UserInterface.java
@@ -1,9 +1,7 @@
 package ru.yandex.practicum.filmorate.Interface;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import ru.yandex.practicum.filmorate.model.User;
 
-import javax.validation.Valid;
 import java.util.Collection;
 
 public interface UserInterface {
